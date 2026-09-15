d6193e8876d98689086c053c822b7324c0c05032
ryanminds7-ui <>
2026-09-15 07:29:41 +0000

    Variable Assignment

---
+++ variables_and_types/variable_assignment/main.py
@@ -0,0 +1,9 @@
+item_name= "Eggs"
+item_price= 3.99 
+item_stock_quantity = 120
+in_stock = True
+
+print("item:",item_name)
+print("Price : $ ",item_price)
+print("Quantity:", item_stock_quantity)
+print("In stock:", in_stock)
